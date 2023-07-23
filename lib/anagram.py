de6ee0845ae4178ca21d031bb11f7ad1bca3b7d8
Seong Kim <runmath@gmail.com>
2023-07-22 19:59:48 -0600

    test done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        lttrs = [lttr for lttr in word]
+        self.lttrs_sorted = sorted(lttrs)
+    
+    def match(self, the_list):
+        matches_list = []
+        for item in the_list:
+            the_lttrs = [lttr for lttr in item]
+            the_lttrs_sorted = sorted(the_lttrs)
+            if the_lttrs_sorted == self.lttrs_sorted:
+                matches_list.append(item)
+        return matches_list
